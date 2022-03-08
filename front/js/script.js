715522e7d2bd0050cb1b72baa2892bf803188808
crist51 <91397701+crist51@users.noreply.github.com>
2022-03-08 12:42:44 +0100

    index ok product en cours

--- front/js/script.js
+++ front/js/script.js
@@ -1,14 +1,16 @@
 fetch("http://localhost:3000/api/products")
 .then(function(response){
   return response.json();
-}).then(function(data){
-  console.log(data);
+}).then(function(reponceAPI){
+  console.log(reponceAPI);
+
+  /*ligneObjetProduct*/
 
 var ligneObjetProduct = 0;//partit de objet concerné
 while (ligneObjetProduct < [8]){
 //--------------création lien index href
   var lienProduct = document.createElement("a");
-  lienProduct.href = "product.html?id="+data[ligneObjetProduct]._id;
+  lienProduct.href = "product.html?id="+ reponceAPI[ligneObjetProduct]._id;
   lienProduct.title = "lien vers canape;"
   items.appendChild(lienProduct)
 
@@ -19,23 +21,23 @@ while (ligneObjetProduct < [8]){
 //--------------création image index
    const numberImgKanap = ligneObjetProduct +1;
       var kanapImg = document.createElement("img");
-          kanapImg.src = "../../back/images/kanap"+ "0" + numberImgKanap +".jpeg";
-          kanapImg.alt = (data[ligneObjetProduct].altTxt);
+          kanapImg.src = "../../back/images/kanap0" + numberImgKanap +".jpeg";
+          kanapImg.alt = (reponceAPI[ligneObjetProduct].altTxt);
       article.appendChild(kanapImg);
 
 //--------------création productName
       var kanapName = document.createElement("h3");
-          kanapName.textContent = ((data[ligneObjetProduct].name))
+          kanapName.textContent = ((reponceAPI[ligneObjetProduct].name))
           kanapName.classList.add("productName");
       article.appendChild(kanapName);
 
 //--------------création kanapDescription
       var kanapDescription = document.createElement("p");
           kanapDescription.classList.add("productDescription");
-          kanapDescription.textContent = ((data[ligneObjetProduct].description));
+          kanapDescription.textContent = ((reponceAPI[ligneObjetProduct].description));
       article.appendChild(kanapDescription);
 
 //--------------changement de ligne du tableau
-      ligneObjetProduct++;
+ligneObjetProduct++;
 }
 });
