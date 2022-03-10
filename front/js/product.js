75a6b565e663ae41cfd05f195baef9b015490f41
crist51 <crist.degorre@gmail.com>
2022-03-10 23:51:39 +0100

    produit base fini
    sans if et else

--- front/js/product.js
+++ front/js/product.js
@@ -13,9 +13,10 @@ fetch("http://localhost:3000/api/products/"+ id)
 .then(function(reponceAPI){
   console.log(reponceAPI);
 
+
   //--------------création lien img
   var kanapImg = document.createElement("img");
-  kanapImg.src =(reponceAPI.imageUrl);
+  kanapImg.src = (reponceAPI.imageUrl);
   kanapImg.alt = (reponceAPI.altTxt);
   kanapImg.classList.add("item__img");
   item__img.appendChild(kanapImg);
@@ -26,7 +27,7 @@ fetch("http://localhost:3000/api/products/"+ id)
   description.textContent = (reponceAPI.description);
 
 //-----------------------------color
-var color= 1
+var color= 0
 while (color < reponceAPI.colors.length) {
     var colorOption = document.createElement("option");//création d'option
     colorOption.value = color;//ajout de la valeur sur l'option
@@ -36,16 +37,6 @@ while (color < reponceAPI.colors.length) {
     color++;
   }
 
-
-const select = document.getElementById("colors");
-console.log(select);
-//const valeur = select.value;console.log(colors.value);
-const colorChoosen = select.value;
-console.log(colorChoosen);
-//////////////////////////////////////////
-//Probleme arrive pas a recup la saisie//
-////////////////////////////////////////
-var alpha = select.value
 //-------------------------quantity
 var input = document.getElementById("quantity").value;
 
@@ -58,22 +49,54 @@ console.log(addToCart);
 //écouter le btn et envoyer dans le panier
 envoyerPanier.addEventListener("click",(event)=>{
   event.preventDefault();
-  //id formulaire
-
+    
+  //-------------------------couleur chsoisit
+  var colorSelect = (reponceAPI.colors[colors.value])//color corespond a la select
+  console.log(colorSelect);
+  //-------------------------quantité voulut
+  var quantity = document.getElementById("quantity").value
+  console.log(quantity);
   //-------------------------recuperation des valeur
+
   const panier = {//optionsProduit
-    "color" : colorChoosen,
+    "color" : colorSelect,
     "nom" : reponceAPI.name,
     "price" : reponceAPI.price,
     "_id" : reponceAPI._id,
-    "quanity" : input,
-    "var2":select.value,
-    //"var4": select(value),
-    "var1":colors.value,
-    //"var3": colors(value),
+    "quanity" : quantity,
     };
   console.log(panier);
-})
 
+  //-------------------------local storage
+//stoker les et recup les donné local storage
+
+//declare la var produitEnreistrer
+let produitEnregistrerDansLeLocalStorage = JSON.parse(localStorage.getItem("key"));
+//JSON.parse pour convertir JSON en JAVASCIPT
+console.log(produitEnregistrerDansLeLocalStorage);
+
+//popUP création
+const popUp = () =>{
+  if(window.confirm('{reponceAPI.name} option:" ${colorSelect} a bien été ajouté au panier consulter le panier OK ou ANNULER afin de continuer vos achat')){
+  window.location.href = "http://127.0.0.1:5500/front/html/cart.html";//----A MODIFIER
+  }
+}
+//si y a des produit dans le local storage sinon le creer
+if(produitEnregistrerDansLeLocalStorage){
+  produitEnregistrerDansLeLocalStorage.push(panier);
+  localStorage.setItem("key",JSON.stringify(produitEnregistrerDansLeLocalStorage));
+  //JSON.stringify convertit le JAVASCRIPT en JSON
+  console.log(produitEnregistrerDansLeLocalStorage);
+  popUp();
+}
+else{
+  produitEnregistrerDansLeLocalStorage = [];
+  produitEnregistrerDansLeLocalStorage.push(panier)
+  localStorage.setItem("key",JSON.stringify(produitEnregistrerDansLeLocalStorage));
+  console.log(produitEnregistrerDansLeLocalStorage);
+  popUp();
+}
+
+});
 
 });
