a4eadbcc8a41490b025deb734254f02580e9492d
crist51 <91397701+crist51@users.noreply.github.com>
2022-03-11 17:11:40 +0100

    cart mplementation price & quantity

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,9 +4,9 @@ console.log(produitEnregistrerDansLeLocalStorage);
 
 console.log();
 
-
 var key = produitEnregistrerDansLeLocalStorage
-console.log(key.length);
+
+
 //---------------boucle de construction
 var debutBoucle = 0
 while (debutBoucle<key.length){
@@ -81,58 +81,31 @@ cart__items.appendChild(article);
   debutBoucle++;
 }
 
-//prixTotal
-//const b = {
-//while (a < key.length){
-    //console.log((key[a].price)*(key[a].quanity));
-   // a++
-//}
-//}
-//tableau pour stoker mes valeur
-
-
 var a = 0
 let quantityArray =[];//on creer un tableau qui aura les quantité de chaque article
 while (a < key.length){
-    console.log(key[a].quantity)
     quantityArray.push(key[a].quantity)//on injecte dans le tableau
   a++
 }
-const reducer = (accumulator,curenceValue) => accumulator + curenceValue
-const quantiteTotal = quantityArray.reduce(reducer);
 console.log(quantityArray);
-console.log(quantiteTotal);
+
+var reducer = (accumulator,curenceValue) => accumulator + curenceValue
+const quantiteTotal = quantityArray.reduce(reducer);
 
 totalQuantity.textContent = quantiteTotal
 
 
-console.log("panier");
+let priceArray =[];//on creer un tableau qui aura les quantité de chaque article
+var v = 0
+while (v < key.length){
+  var priceTotalArticle = key[v].price * quantityArray[v]//quantiter * prix
+  priceArray.push(priceTotalArticle)//on injecte dans le tableau
+  v++
+}
+console.log(priceArray);
+var reducerPrice = (accumulator,curenceValue) => accumulator + curenceValue
+const priceTotal = priceArray.reduce(reducerPrice);
 
-
-// var b = 1
-// console.log(key[b].price);
-// console.log(quantityArray[b]);
-// var priceTotalArticle = key[b].price * quantityArray[b]
-// console.log(priceTotalArticle);
-
-
-// let priceArray =[];//on creer un tableau qui aura les quantité de chaque article
-// var v = 0
-// console.log(v)
-// while (v < key.length){
-//   console.log(key[v].price);//valleur prix traité
-//   console.log(quantityArray[v]);//valleur quantité traité
-//   var priceTotalArticle = key[v].price * quantityArray[v]//quantiter * prix
-//   console.log(priceTotalArticle);
-//   priceArray.push(key[a].priceTotalArticle)//on injecte dans le tableau
-//   v++
-// }
-// const reducerPrice = (accumulator,curenceValue) => accumulator + curenceValue
-// const priceTotal = quantityArray.reduce(reducerPrice);
-// console.log(priceArray);
-// console.log(priceTotal);
-
-
-//totalPrice.txtContent = priceTotal
+totalPrice.textContent = priceTotal
 
 //file:///E:/NE%20PAS%20EFFACER/Stockage/Nouveau%20dossier/P5_degorre/front/html/cart.html
