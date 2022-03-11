94a1f796d1111f8fa96c84edd75070efb6c6eb0d
crist51 <91397701+crist51@users.noreply.github.com>
2022-03-11 16:29:49 +0100

    01

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,54 +1,138 @@
-//EXEMPLE 1 non bon
-/**Retourne la valeur du select selectId*/
-// function getSelectValue(selectId)
-// {
-// 	/**On récupère l'élement html <select>*/
-// 	var selectElmt = document.getElementById(selectId);
-// 	/**
-// 	selectElmt.options correspond au tableau des balises <option> du select
-// 	selectElmt.selectedIndex correspond à l'index du tableau options qui est actuellement sélectionné
-// 	*/
-// 	return selectElmt.options[selectElmt.selectedIndex].value;
+
+let produitEnregistrerDansLeLocalStorage = JSON.parse(localStorage.getItem("key"));//JSON.parse pour convertir JSON en JAVASCIPT
+console.log(produitEnregistrerDansLeLocalStorage);
+
+console.log();
+
+
+var key = produitEnregistrerDansLeLocalStorage
+console.log(key.length);
+//---------------boucle de construction
+var debutBoucle = 0
+while (debutBoucle<key.length){
+  var article = document.createElement("article");
+article.classList.add("cart__item")
+cart__items.appendChild(article);
+
+    //création div [img]
+    var cart__item__img
+    = document.createElement("div")
+    cart__item__img.classList.add("cart__item__img")
+    article.appendChild(cart__item__img)
+    
+        var image =document.createElement("img")
+        image.src = (key[debutBoucle].imageUrl)
+        image.alt = (key[debutBoucle].altTxt)
+        cart__item__img.appendChild(image)
+
+        //partit1 création div [non],[option],[price],
+        var cart__item__content = document.createElement("div")
+        cart__item__content.classList.add("cart__item__content")
+        article.appendChild(cart__item__content)
+
+            var cart__item__content__description = document.createElement("div")
+            cart__item__content__description.classList.add("cart__item__content__description")
+            cart__item__content.appendChild(cart__item__content__description)
+
+                var nom = document.createElement("h2")
+                nom.textContent = (key[debutBoucle].name)
+                cart__item__content__description.appendChild(nom)
+
+                var colors = document.createElement("p")
+                colors.textContent = (key[debutBoucle].colors)
+                cart__item__content__description.appendChild(colors)
+
+                var price = document.createElement("p")
+                    price.textContent = (key[debutBoucle].price)
+                cart__item__content__description.appendChild(price)
+
+                  //--------------partit2 quantité
+                var cart__item__content__seetings = document.createElement("div")
+                cart__item__content__seetings.classList.add("cart__item__content__settings")
+                cart__item__content.appendChild(cart__item__content__seetings)
+
+                      //--------------quantité
+                      var description = document.createElement("div")
+                      description.classList.add("cart__item__content__settings__quantity")
+                      cart__item__content__seetings.appendChild(description)
+
+                      var qte = document.createElement("p")
+                      qte.textContent = "qté";
+                      description.appendChild(qte)
+
+                      var quantite = document.createElement("input");
+                      quantite.type = "number"
+                      quantite.name = "itemQuantity"
+                      quantite.min = "1"
+                      quantite.max = "100"
+                      quantite.value = (key[debutBoucle].quantity)
+                      quantite.classList.add("itemQuantity")
+                      description.appendChild(quantite)
+
+                      //--------------supprimer
+                      var seeting = document.createElement("div")
+                      seeting.classList.add("cart__item__content__settings__delete")
+                      cart__item__content__seetings.appendChild(seeting)
+
+                      var sup = document.createElement("p")
+                      sup.classList.add("deleteItem")
+                      sup.textContent = "Supprimer"
+                      seeting.appendChild(sup)
+  debutBoucle++;
+}
+
+//prixTotal
+//const b = {
+//while (a < key.length){
+    //console.log((key[a].price)*(key[a].quanity));
+   // a++
+//}
+//}
+//tableau pour stoker mes valeur
+
+
+var a = 0
+let quantityArray =[];//on creer un tableau qui aura les quantité de chaque article
+while (a < key.length){
+    console.log(key[a].quantity)
+    quantityArray.push(key[a].quantity)//on injecte dans le tableau
+  a++
+}
+const reducer = (accumulator,curenceValue) => accumulator + curenceValue
+const quantiteTotal = quantityArray.reduce(reducer);
+console.log(quantityArray);
+console.log(quantiteTotal);
+
+totalQuantity.textContent = quantiteTotal
+
+
+console.log("panier");
+
+
+// var b = 1
+// console.log(key[b].price);
+// console.log(quantityArray[b]);
+// var priceTotalArticle = key[b].price * quantityArray[b]
+// console.log(priceTotalArticle);
+
+
+// let priceArray =[];//on creer un tableau qui aura les quantité de chaque article
+// var v = 0
+// console.log(v)
+// while (v < key.length){
+//   console.log(key[v].price);//valleur prix traité
+//   console.log(quantityArray[v]);//valleur quantité traité
+//   var priceTotalArticle = key[v].price * quantityArray[v]//quantiter * prix
+//   console.log(priceTotalArticle);
+//   priceArray.push(key[a].priceTotalArticle)//on injecte dans le tableau
+//   v++
 // }
-// var selectValue = getSelectValue('identifiantDeMonSelect');
-// console.log(selectValue);
-//
+// const reducerPrice = (accumulator,curenceValue) => accumulator + curenceValue
+// const priceTotal = quantityArray.reduce(reducerPrice);
+// console.log(priceArray);
+// console.log(priceTotal);
 
 
-//2 ex fonctionne
-/*
-function Recup_select_info(obj,choix_rech){
-    var idx = obj.selectedIndex;
-    if((choix_rech) && (choix_rech == 'valeur')){ return obj.options[idx].value;} // récupère valeur du select
-    else if((choix_rech) && (choix_rech == 'texte')){ return obj.options[idx].innerHTML;} // récupère le contenu html du select
-    else{ return idx;} // récupère l'index de position dans le tableau select
-}
-console.log(Recup_select_info);
+//totalPrice.txtContent = priceTotal
 
-function remplissageAuto(obj) {
-    var valeur_alias = Recup_select_info(obj);
-    valeur_alias += '\n\n'+Recup_select_info(obj,'valeur');
-    valeur_alias += '\n\n'+Recup_select_info(obj,'texte');
-    alert(valeur_alias);
-    console.log(remplissageAuto);
-}
-*/
-
-var selectElmt = document.getElementById("ComboPays");
-var valeurselectionnee = selectElmt.options[selectElmt.selectedIndex].value;
-var textselectionne = selectElmt.options[selectElmt.selectedIndex].text;
-console.log(valeurselectionnee);//value
-console.log(textselectionne);
-function Recup_select_info(obj,choix_rech){
-    var idx = obj.selectedIndex;
-    if((choix_rech) && (choix_rech == 'valeur')){ return obj.options[idx].value;} // récupère valeur du select
-    else if((choix_rech) && (choix_rech == 'texte')){ return obj.options[idx].innerHTML;} // récupère le contenu html du select
-    else{ return idx;} // récupère l'index de position dans le tableau select
-};
-
-function remplissageAuto(obj) {
-    var valeur_alias = Recup_select_info(obj);
-    //valeur_alias += Recup_select_info(obj,'valeur');
-    //valeur_alias += Recup_select_info(obj,'texte');
-    console.log(valeur_alias);
-}
+//file:///E:/NE%20PAS%20EFFACER/Stockage/Nouveau%20dossier/P5_degorre/front/html/cart.html
--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+
 //recuperation id
 var str = window.location.href
 console.log(str);
@@ -13,20 +14,22 @@ fetch("http://localhost:3000/api/products/"+ id)
 .then(function(reponceAPI){
   console.log(reponceAPI);
 
+
   //--------------création lien img
-  var kanapImg = document.createElement("img");
-  kanapImg.src =(reponceAPI.imageUrl);
+  const kanapImg = document.createElement("img");
+  kanapImg.src = (reponceAPI.imageUrl);
   kanapImg.alt = (reponceAPI.altTxt);
   kanapImg.classList.add("item__img");
   item__img.appendChild(kanapImg);
 
+
   //--------------création title & price & description
   title.textContent = (reponceAPI.name);
-  price.textContent = (reponceAPI.price)/10;
+  price.textContent = (reponceAPI.price);
   description.textContent = (reponceAPI.description);
 
 //-----------------------------color
-/*var color= 1
+var color= 0
 while (color < reponceAPI.colors.length) {
     var colorOption = document.createElement("option");//création d'option
     colorOption.value = color;//ajout de la valeur sur l'option
@@ -34,96 +37,7 @@ while (color < reponceAPI.colors.length) {
     console.log(reponceAPI.colors[color]);
     colors.appendChild(colorOption);//On ajoute option dans le HTML;
     color++;
-    console.log(colorOption.value)
   }
-  */
-  var color= 1
-while (color < reponceAPI.colors.length) {
-    var colorOption = document.createElement("option");//création d'option
-    colorOption.value = color;//ajout de la valeur sur l'option
-    colorOption.text = (reponceAPI.colors[color]);  //ajout du texte dans l'option
-    console.log(reponceAPI.colors[color]);
-    colors.appendChild(colorOption);//On ajoute option dans le HTML;
-    color++;
-}
-
-//select id 
-var selecId = document.getElementById("colors");
-console.log(selecId);
-//valleur de l'option selectionne
-console.log(selecId[3]);//choisit la ligne option
-var a = 0;//valeur entre crocher
-console.log(selecId[a]);
-var selectOption = selecId.value
-
-
-/* faudrais que je recup le => []*/
-
-/*
-var selectOption = selecId.value;
-console.log(selectOption);
-var e = colors.select
-console.log(e);
-// non de la couleur
-var selectColor = reponceAPI.colors[selectOption];//definit la couleur
-console.log(selectColor); //nomme la couleur
-*/
-
-const ab = selectOption ;//racoucis panoer
-/*
-  const idForm = document.querySelector("#colors")
-  console.log(reponceAPI.colors);
-  const select = document.getElementById(idForm)
-*/
-////////////////////////////////////////////////////
-//    Probleme arrive pas a recup la saisie      //
-//////////////////////////////////////////////////
-
-
-/*
-function Recup_select_info(obj,choix_rech){
-  var idx = obj.selectedIndex;
-  if((choix_rech) && (choix_rech == 'valeur')){ return obj.options[idx].value;} // récupère valeur du select
-  else if((choix_rech) && (choix_rech == 'texte')){ return obj.options[idx].innerHTML;} // récupère le contenu html du select
-  else{ return idx;} // récupère l'index de position dans le tableau select
-}
-*/
-/*
-function remplissageAuto(obj) {
-  var valeur_alias = Recup_select_info(obj);
-  valeur_alias += '\n\n'+Recup_select_info(obj,'valeur');
-  valeur_alias += '\n\n'+Recup_select_info(obj,'texte');
-  alert(valeur_alias);
-}
-*/
-
-/*
-function Recup_select_info(obj,choix_rech){
-  var idx = obj.selectedIndex;
-  if((choix_rech) && (choix_rech == 'valeur')){ return obj.options[idx].value;} // récupère valeur du select
-  else if((choix_rech) && (choix_rech == 'texte')){ return obj.options[idx].innerHTML;} // récupère le contenu html du select
-  else{ return idx;} // récupère l'index de position dans le tableau select
-}
-console.log(Recup_select_info);
-
-function remplissageAuto(obj) {
-  var valeur_alias = Recup_select_info(obj);
-  valeur_alias += '\n\n'+Recup_select_info(obj,'valeur');
-  valeur_alias += '\n\n'+Recup_select_info(obj,'texte');
-  alert(valeur_alias);
-}
-console.log(remplissageAuto);
-*/
-
-
-
-
-
-
-//-------------------------quantity
-//var quantity = document.getElementById("quantity").value;
-console.log(quantity);
-var inputQuantity = document.getElementById("quantity");
 
 //-------------------------La gestion du panier
 
@@ -133,18 +47,53 @@ console.log(addToCart);
 //écouter le btn et envoyer dans le panier
 envoyerPanier.addEventListener("click",(event)=>{
   event.preventDefault();
-  //id formulaire
+    
+  //-------------------------couleur choisit
+  var colorSelect = (reponceAPI.colors[colors.value])//color corespond a la select
+  console.log(colorSelect);
 
-  
+  //-------------------------quantité voulut
+  var quantity =Number (document.getElementById("quantity").value)//rajout Number
+  console.log(quantity);
+
   //-------------------------recuperation des valeur
+
   const panier = {//optionsProduit
-    "color" : ab,
-    "nom" : reponceAPI.name,
-    "price" : reponceAPI.price/10,
+    "colors" : colorSelect,
     "_id" : reponceAPI._id,
-    "quanity" : (inputQuantity.value),
+    "name" : reponceAPI.name,
+    "price" : reponceAPI.price,
+    "imageUrl" : reponceAPI.imageUrl,
+    "altTxt": (reponceAPI.altTxt),
+    "quantity" : quantity,
     };
   console.log(panier);
-})
+
+//-------------------------local storage
+
+let produitEnregistrerDansLeLocalStorage = JSON.parse(localStorage.getItem("key"));//JSON.parse pour convertir JSON en JAVASCIPT
+
+//popUP création
+const popUp = () =>{
+  // if(window.confirm("{reponceAPI.name} option: ${colorSelect} a bien été ajouté au panier consulter le panier OK ou ANNULER afin de continuer vos achat")){
+    if(window.confirm('"'+ (reponceAPI.name) +'"' + " couleur : " + (colorSelect) + " a bien été ajouté au panier consulter le panier OK ou ANNULER afin de continuer vos achat")){
+  window.location.href = "http://127.0.0.1:5500/front/html/cart.html";//----A MODIFIER
+  }
+}
+
+if(produitEnregistrerDansLeLocalStorage){//si y a des produit dans le local storage on rajoute un objet 
+  produitEnregistrerDansLeLocalStorage.push(panier);
+  localStorage.setItem("key",JSON.stringify(produitEnregistrerDansLeLocalStorage));
+  //JSON.stringify convertit le JAVASCRIPT en JSON
+  popUp();
+}
+else{
+  produitEnregistrerDansLeLocalStorage = [];//sinon on creer  la clef
+  produitEnregistrerDansLeLocalStorage.push(panier)
+  localStorage.setItem("key",JSON.stringify(produitEnregistrerDansLeLocalStorage));
+  popUp();
+}
+
+});
 
 });
