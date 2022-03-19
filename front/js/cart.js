6db23085399ba975fdcf3a594b9f4f756a014165
crist51 <91397701+crist51@users.noreply.github.com>
2022-03-19 04:03:24 +0100

    1

--- front/js/cart.js
+++ front/js/cart.js
@@ -78,6 +78,9 @@ else {
     quantite.classList.add("itemQuantity")
     description.appendChild(quantite)
 
+
+
+
     //----------------------------------------------------------------création div = cart__item__content__settings__delete[delete]
     var seeting = document.createElement("div")
     seeting.classList.add("cart__item__content__settings__delete")
@@ -88,9 +91,37 @@ else {
     sup.textContent = "Supprimer"
     seeting.appendChild(sup)
 
-
     debutBoucle++
   }
+}
+
+//********************************************************************mise a jour de quantite********************************************************************
+
+// let inputQuantite = document.querySelector(".itemQuantity")
+// console.log(inputQuantite.value);
+// console.log(inputQuantite.value);
+
+//  var p = 0
+//  while (p < key.length) {
+//     let inputQuantite = document.querySelector(".itemQuantity")
+//     inputQuantite.addEventListener("change", function () {
+//       inputModif(this);
+//     })
+//     const inputModif = function (inputQuantiteValue) {
+//       var valeurInputModif = inputQuantite.value
+//       if (inputQuantite.value == quantite.value) {
+//         quantite.value = valeurInputModif
+//         console.log(inputQuantite.value + " a changer");
+//       }
+//       return quantite.value
+//     }
+//     p++
+//   }
+
+// var qte = m.closest(1)
+// console.log(qte);
+//Element.closest()
+//********************************************************************btn sup********************************************************************
 
   // var p = 0
   // console.log(p);
@@ -106,258 +137,260 @@ else {
   //   })
   //   p++
   // }
-
-  console.log(key[1]);
-
-
-  //********************************************************************les calcul quantité & price********************************************************************
-
-  //--------------------------------quantity
-  var debutBoucle = 0
-  let quantityArray = [];
-  while (debutBoucle < key.length) {
-    quantityArray.push(/*key[debutBoucle].quantity*/quantite.value)
-    debutBoucle++
-  }
-
-  var reducer = (accumulator, curenceValue) => accumulator + curenceValue
-  const quantiteTotal = quantityArray.reduce(reducer);
-
-  totalQuantity.textContent = quantiteTotal
-
-  //--------------------------------price
-  let priceArray = [];
-  var debutBoucle = 0
-  while (debutBoucle < key.length) {
-    var priceTotalArticle = key[debutBoucle].price * quantityArray[debutBoucle]//quantiter * prix
-    priceArray.push(priceTotalArticle)
-    debutBoucle++
-  }
-  var reducerPrice = (accumulator, curenceValue) => accumulator + curenceValue
-  const priceTotal = priceArray.reduce(reducerPrice);
-
-  totalPrice.textContent = priceTotal
-
-
-  //********************************************************************reg Exp********************************************************************
-
-  //--------------------------------rexExp firstName
-  let firstName = document.getElementById("firstName")
-  firstName.addEventListener("change", function () {
-    validFirstName(this);
-  })
-  const validFirstName = function (firstName) {
-    let testFirsName = false
-    //commencez avec une maj
-    if (!/^[A-Z]/.test(firstName.value)) {//! si il n'y as pas c'est faux & debut
-      firstNameErrorMsg.textContent = "Commencez avec une majuscule"
-      return false
-    }
-    //au mois 3 caractere
-    else if (firstName.value.length < 2) {
-      firstNameErrorMsg.textContent = "Pas assez de caratere"
-      return false
-    }
-    //au moins une minuscule
-    else if (!/[a-z]/.test(firstName.value)) {
-      firstNameErrorMsg.textContent = "Il manque une minuscule"
-      return false
-    }
-    else if (firstName.value.length > 45) {
-      firstNameErrorMsg.textContent = "Il y a trop de caractere"
-      return false
-    }
-    //firstName valide
-    else {
-      testFirsName = true
-      return true
-    }
-  }
-
-  //--------------------------------rexExp lastName
-  let lastName = document.getElementById("lastName")
-  lastName.addEventListener("change", function () {
-    validLastName(this);
-  })
-  const validLastName = function (lastName) {
-    let testlastName = false
-    //commencez avec une maj
-    if (!/^[A-Z]/.test(lastName.value)) {
-      lastNameErrorMsg.textContent = "Commencez avec une majuscule"
-      return false
-    }
-    //au mois 3 caractere
-    else if (lastName.value.length < 2) {//inputPasword
-      lastNameErrorMsg.textContent = "Pas assez de caratere"
-      return false
-    }
-    //au moins une minuscule
-    else if (!/[a-z]/.test(lastName.value)) {
-      lastNameErrorMsg.textContent = "Il manque une minuscule"
-      return false
-    }
-    else if (lastName.value.length > 45) {
-      lastNameErrorMsg.textContent = "Il y a trop de caractere"
-      return false
-      //lastName valide
-    }
-    else {
-      testlastName = true
-      return true
-    }
-  }
-
-  //--------------------------------rexExp address
-  let address = document.getElementById("address")
-  address.addEventListener("change", function () {
-    validAddress(this);
-  })
-  const validAddress = function (address) {
-    let testAddress = false
-    //commencez avec une maj
-    //au mois 3 caractere
-    if (address.value.length < 2) {
-      addressErrorMsg.textContent = "Pas assez de caratere"
-      return false
-    }
-    //au moins une minuscule
-    else if (!/[a-z]/.test(address.value)) {
-      addressErrorMsg.textContent = "Il manque une minuscule"
-      return false
-    }
-    else if (address.value.length > 45) {
-      addressErrorMsg.textContent = "Il y a trop de caractere"
-      return false
-    }
-    //mdp valide
-    else {
-      testAddress = true
-      return true
-    }
-  }
-
-  //--------------------------------rexExp city
-  let city = document.getElementById("city")
-  city.addEventListener("change", function () {
-    validCity(this);
-  })
-  const validCity = function (city) {
-    let testCity = false
-    //commencez avec une maj
-    if (!/^[A-Z]/.test(lastName.value)) {
-      cityErrorMsg.textContent = "Commencez avec une majuscule"
-      return false
-    }
-    else if (city.value.length < 2) {//inputPasword
-      cityErrorMsg.textContent = "Pas assez de caratere"
-      return false
-    }
-    //au moins une minuscule
-    else if (!/[a-z]/.test(city.value)) {
-      cityErrorMsg.textContent = "Il manque une minuscule"
-      return false
-    }
-    //nombre dee caractere limit
-    else if (city.value.length > 45) {
-      cityErrorMsg.textContent = "Il y a trop de caractere"
-      return false
-    }
-    //city valide
-    else {
-      testCity = true
-      return true
-    }
-  }
-
-  //--------------------------------rexExp email
-  let email = document.getElementById("email")
-  email.addEventListener("change", function () {
-    validEmail(this)
-  })
-  const validEmail = function (inputEmail) {
-    //création regExp  E-mail
-    let emailRegExp = new RegExp(
-      "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$",
-      //"g"
-      //  ^[] au début on doit commencer a → z ou A → Z ou 0 →9 ou on oeut metre des . - _ et le  + signifit que l'on peut les utiliser plusieur fois
-      //  [@]{1} un doit avoir un @ 1 fois
-      //$ fin de la RegEX (expression reguliere)
-    )
-
-    //--------------------------------affichage si pb mail
-    let testEmail = emailRegExp.test(inputEmail.value)
-    //console.log(testEmail);
-
-    if (testEmail == true) {
-      return true
-    }
-    else {
-      emailErrorMsg.textContent = "Adresse Mail Non Valide"
-      return false
-    }
-  }
-    ;
+  
 
 
 
 
-  ////********************************************************************La gestion de commande//********************************************************************
 
-  //selectionner le btn dans le dom
-  const Commander = document.querySelector("#order")
-  //écouter le btn et envoyer dans le panier
-  Commander.addEventListener("click", (event) => {
-    event.preventDefault();
+//********************************************************************les calcul quantité & price********************************************************************
 
-
-    //--------------------------------condition d'envoie de formulaire 
-
-
-
-    console.log(validEmail(email))
-    console.log(validCity(city));
-    console.log(validAddress(address));
-    console.log(validLastName(lastName));
-    console.log(validFirstName(firstName));
-
-    if (validEmail(email) && validCity(city) && validAddress(address) && validLastName(lastName) && validFirstName(firstName) == true) {
-
-
-
-      //--------------------------------commande
-      const Command = {
-        "firstName": firstName.value,
-        "lastName": lastName.value,
-        "address": address.value,
-        "price": city.value,
-        "city": city.value,
-        "email": email.value,
-        "panier": produitEnregistrerDansLeLocalStorage,
-      };
-      console.log(Command);
-
-      ////********************************************************************local storage//********************************************************************
-
-      let CommandDansLeLocalStorage = JSON.parse(localStorage.getItem("key"));//JSON.parse pour convertir JSON en JAVASCIPT
-
-      //--------------------------------pop-UP de validation
-      const popUp = () => {
-        if (window.confirm((lastName.value) + " " + (firstName.value) + "\n" + "Votre commande a bien été pris en compte pour la validé cliqué sur OK" + "\n" + "ou pour l\'annuler cliquée sur \"annuler\""))
-          window.location.href = "http://127.0.0.1:5500/front/html/confirmation.html";//----A MODIFIER
-      }
-
-      if (CommandDansLeLocalStorage) {//si y a des produit dans le local storage on rajoute un objet 
-        CommandDansLeLocalStorage.push(Command);
-        localStorage.setItem("CommandEnregistre", JSON.stringify(CommandDansLeLocalStorage));
-        popUp();
-      }
-      else {
-        CommandDansLeLocalStorage = [];//sinon on creer  le tableau vide
-        CommandDansLeLocalStorage.replace(Command)//on creer la clef
-        localStorage.setItem("CommandEnregistre", JSON.stringify(CommandDansLeLocalStorage));
-        popUp();
-      }
-    }
-
-  });
+//--------------------------------quantity
+var debutBoucle = 0
+let quantityArray = [];
+while (debutBoucle < key.length) {
+  quantityArray.push(/*key[debutBoucle].quantity*/quantite.value)
+  debutBoucle++
 }
+
+var reducer = (accumulator, curenceValue) => accumulator + curenceValue
+const quantiteTotal = quantityArray.reduce(reducer);
+
+totalQuantity.textContent = quantiteTotal
+
+//--------------------------------price
+let priceArray = [];
+var debutBoucle = 0
+while (debutBoucle < key.length) {
+  var priceTotalArticle = key[debutBoucle].price * quantityArray[debutBoucle]//quantiter * prix
+  priceArray.push(priceTotalArticle)
+  debutBoucle++
+}
+var reducerPrice = (accumulator, curenceValue) => accumulator + curenceValue
+const priceTotal = priceArray.reduce(reducerPrice);
+
+totalPrice.textContent = priceTotal
+
+
+//********************************************************************reg Exp********************************************************************
+
+//--------------------------------rexExp firstName
+let firstName = document.getElementById("firstName")
+firstName.addEventListener("change", function () {
+  validFirstName(this);
+})
+const validFirstName = function (firstName) {
+  let testFirsName = false
+  //commencez avec une maj
+  if (!/^[A-Z]/.test(firstName.value)) {//! si il n'y as pas c'est faux & debut
+    firstNameErrorMsg.textContent = "Commencez avec une majuscule"
+    return false
+  }
+  //au mois 3 caractere
+  else if (firstName.value.length < 2) {
+    firstNameErrorMsg.textContent = "Pas assez de caratere"
+    return false
+  }
+  //au moins une minuscule
+  else if (!/[a-z]/.test(firstName.value)) {
+    firstNameErrorMsg.textContent = "Il manque une minuscule"
+    return false
+  }
+  else if (firstName.value.length > 45) {
+    firstNameErrorMsg.textContent = "Il y a trop de caractere"
+    return false
+  }
+  //firstName valide
+  else {
+    testFirsName = true
+    return true
+  }
+}
+
+//--------------------------------rexExp lastName
+let lastName = document.getElementById("lastName")
+lastName.addEventListener("change", function () {
+  validLastName(this);
+})
+const validLastName = function (lastName) {
+  let testlastName = false
+  //commencez avec une maj
+  if (!/^[A-Z]/.test(lastName.value)) {
+    lastNameErrorMsg.textContent = "Commencez avec une majuscule"
+    return false
+  }
+  //au mois 3 caractere
+  else if (lastName.value.length < 2) {//inputPasword
+    lastNameErrorMsg.textContent = "Pas assez de caratere"
+    return false
+  }
+  //au moins une minuscule
+  else if (!/[a-z]/.test(lastName.value)) {
+    lastNameErrorMsg.textContent = "Il manque une minuscule"
+    return false
+  }
+  else if (lastName.value.length > 45) {
+    lastNameErrorMsg.textContent = "Il y a trop de caractere"
+    return false
+    //lastName valide
+  }
+  else {
+    testlastName = true
+    return true
+  }
+}
+
+//--------------------------------rexExp address
+let address = document.getElementById("address")
+address.addEventListener("change", function () {
+  validAddress(this);
+})
+const validAddress = function (address) {
+  let testAddress = false
+  //commencez avec une maj
+  //au mois 3 caractere
+  if (address.value.length < 2) {
+    addressErrorMsg.textContent = "Pas assez de caratere"
+    return false
+  }
+  //au moins une minuscule
+  else if (!/[a-z]/.test(address.value)) {
+    addressErrorMsg.textContent = "Il manque une minuscule"
+    return false
+  }
+  else if (address.value.length > 45) {
+    addressErrorMsg.textContent = "Il y a trop de caractere"
+    return false
+  }
+  //mdp valide
+  else {
+    testAddress = true
+    return true
+  }
+}
+
+//--------------------------------rexExp city
+let city = document.getElementById("city")
+city.addEventListener("change", function () {
+  validCity(this);
+})
+const validCity = function (city) {
+  let testCity = false
+  //commencez avec une maj
+  if (!/^[A-Z]/.test(lastName.value)) {
+    cityErrorMsg.textContent = "Commencez avec une majuscule"
+    return false
+  }
+  else if (city.value.length < 2) {//inputPasword
+    cityErrorMsg.textContent = "Pas assez de caratere"
+    return false
+  }
+  //au moins une minuscule
+  else if (!/[a-z]/.test(city.value)) {
+    cityErrorMsg.textContent = "Il manque une minuscule"
+    return false
+  }
+  //nombre dee caractere limit
+  else if (city.value.length > 45) {
+    cityErrorMsg.textContent = "Il y a trop de caractere"
+    return false
+  }
+  //city valide
+  else {
+    testCity = true
+    return true
+  }
+}
+
+//--------------------------------rexExp email
+let email = document.getElementById("email")
+email.addEventListener("change", function () {
+  validEmail(this)
+})
+const validEmail = function (inputEmail) {
+  //création regExp  E-mail
+  let emailRegExp = new RegExp(
+    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$",
+    //"g"
+    //  ^[] au début on doit commencer a → z ou A → Z ou 0 →9 ou on oeut metre des . - _ et le  + signifit que l'on peut les utiliser plusieur fois
+    //  [@]{1} un doit avoir un @ 1 fois
+    //$ fin de la RegEX (expression reguliere)
+  )
+
+  //--------------------------------affichage si pb mail
+  let testEmail = emailRegExp.test(inputEmail.value)
+  //console.log(testEmail);
+
+  if (testEmail == true) {
+    return true
+  }
+  else {
+    emailErrorMsg.textContent = "Adresse Mail Non Valide"
+    return false
+  }
+}
+  ;
+
+
+
+
+////********************************************************************La gestion de commande//********************************************************************
+
+//selectionner le btn dans le dom
+const Commander = document.querySelector("#order")
+//écouter le btn et envoyer dans le panier
+Commander.addEventListener("click", (event) => {
+  event.preventDefault();
+
+
+  //--------------------------------condition d'envoie de formulaire 
+
+
+
+  console.log(validEmail(email))
+  console.log(validCity(city));
+  console.log(validAddress(address));
+  console.log(validLastName(lastName));
+  console.log(validFirstName(firstName));
+
+  if (validEmail(email) && validCity(city) && validAddress(address) && validLastName(lastName) && validFirstName(firstName) == true) {
+
+
+
+    //--------------------------------commande
+    const Command = {
+      "firstName": firstName.value,
+      "lastName": lastName.value,
+      "address": address.value,
+      "price": city.value,
+      "city": city.value,
+      "email": email.value,
+      "panier": produitEnregistrerDansLeLocalStorage,
+    };
+    console.log(Command);
+
+    ////********************************************************************local storage//********************************************************************
+
+    let CommandDansLeLocalStorage = JSON.parse(localStorage.getItem("key"));//JSON.parse pour convertir JSON en JAVASCIPT
+
+    //--------------------------------pop-UP de validation
+    const popUp = () => {
+      if (window.confirm((lastName.value) + " " + (firstName.value) + "\n" + "Votre commande a bien été pris en compte pour la validé cliqué sur OK" + "\n" + "ou pour l\'annuler cliquée sur \"annuler\""))
+        window.location.href = "http://127.0.0.1:5500/front/html/confirmation.html";//----A MODIFIER
+    }
+
+    if (CommandDansLeLocalStorage) {//si y a des produit dans le local storage on rajoute un objet 
+      CommandDansLeLocalStorage.push(Command);
+      localStorage.setItem("CommandEnregistre", JSON.stringify(CommandDansLeLocalStorage));
+      popUp();
+    }
+    else {
+      CommandDansLeLocalStorage = [];//sinon on creer  le tableau vide
+      CommandDansLeLocalStorage.replace(Command)//on creer la clef
+      localStorage.setItem("CommandEnregistre", JSON.stringify(CommandDansLeLocalStorage));
+      popUp();
+    }
+  }
+
+});
+//}
